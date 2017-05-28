5e7d0f15504d28639268d381ed76984ec87c6b89
Krzysztof <krzysztof_serkowski@interia.pl>
2017-05-28 19:23:12 +0200

    Gradle

---
+++ src/main/java/com/example/demo/controller/TwitterController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+@Controller
+public class TwitterController {
+    @Autowired
+    private Twitter twitter;
+
+    @RequestMapping("/")
+    public String home(){
+     return "searchPage";
+    }
+
+    @RequestMapping("/result")
+    public String hello(@RequestParam(defaultValue = "WorldOfWarcraft") String search, Model model){
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<Tweet> tweets = searchResults.getTweets();
+        model.addAttribute("tweets", tweets);
+        model.addAttribute("search", search);
+        return "resultPage";
+    }
+}
